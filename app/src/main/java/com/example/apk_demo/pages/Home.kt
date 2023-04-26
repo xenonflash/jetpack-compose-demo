ec2a7f4d6b15c70309cebad10a0f772e1882aff4
xenonflash <jzykc@hotmail.com>
2023-04-26 19:33:06 +0800

    home page 添加 splash 屏

--- app/src/main/java/com/example/apk_demo/pages/Home.kt
+++ app/src/main/java/com/example/apk_demo/pages/Home.kt
@@ -1,15 +1,23 @@
 package com.example.apk_demo.pages
 
+import android.window.SplashScreen
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
@@ -17,26 +25,36 @@ import com.airbnb.lottie.compose.LottieDynamicProperties
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.apk_demo.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun HomePage(nav: NavController) {
-    Surface() {
-        Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+    var showSplash by remember {
+        mutableStateOf(true)
+    }
+    Box {
+        LaunchedEffect(key1 = "hide splash screen", block = {
+            delay(1500)
+            showSplash = false
+        })
+        if (showSplash) SplashScreen() else Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
             Text("Home")
             Button(onClick = { nav.navigate("login") }) {
                 Text("跳转回登陆")
             }
-            Loader()
         }
     }
 }
 
+
 @Composable
-fun Loader() {
+fun SplashScreen() {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
     val progress by animateLottieCompositionAsState(composition)
-    LottieAnimation(
-        composition = composition,
-        progress = { progress }
-    )
+    Column(Modifier.fillMaxSize().background(Color.White), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        LottieAnimation(
+            composition = composition,
+            progress = { progress }
+        )
+    }
 }
