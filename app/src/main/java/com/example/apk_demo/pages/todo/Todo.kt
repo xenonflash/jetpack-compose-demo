01402e365b6aa4a373bf8095ebdb1269fcada918
xenonflash <jzykc@hotmail.com>
2023-07-10 18:23:44 +0800

    add keyboard config

--- app/src/main/java/com/example/apk_demo/pages/todo/Todo.kt
+++ app/src/main/java/com/example/apk_demo/pages/todo/Todo.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -44,11 +46,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import java.util.UUID
@@ -125,10 +130,11 @@ fun TodoItem(data: TodoItemData, handleCheck: (id: UUID) -> Unit, handleRemove: 
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun NewItemInput(modifier: Modifier = Modifier, handleAdd: (data: TodoItemData) -> Unit) {
     val ctx = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
     var text by remember {
         mutableStateOf("")
     }
@@ -183,7 +189,14 @@ fun NewItemInput(modifier: Modifier = Modifier, handleAdd: (data: TodoItemData) 
                 modifier = Modifier
                     .padding(end = 10.dp),
                 colors = TextFieldDefaults.textFieldColors(containerColor = Color.Transparent),
-                maxLines = 1
+                maxLines = 1,
+                // 配置软键盘
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                // 收起键盘
+                keyboardActions = KeyboardActions(onDone = {
+                    handleItemAdd()
+                    keyboardController?.hide()
+                })
             )
             Button(onClick = {handleItemAdd()}, enabled = text.isNotBlank()) {
                 Text(text = "添加")
