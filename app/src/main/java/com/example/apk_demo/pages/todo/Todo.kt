8341313a41638d22e6e8226fb102494df2a64677
xenonflash <jzykc@hotmail.com>
2023-07-10 17:40:00 +0800

    todo add input

--- app/src/main/java/com/example/apk_demo/pages/todo/Todo.kt
+++ app/src/main/java/com/example/apk_demo/pages/todo/Todo.kt
@@ -1,28 +1,51 @@
 package com.example.apk_demo.pages.todo
 
+import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import java.util.UUID
 
@@ -53,12 +76,7 @@ fun TodoMain(nav: NavHostController, store: TodoViewModel) {
             .fillMaxSize()
     ) {
         TodoList(store.todoItems.observeAsState(listOf()), store)
-        Button(
-            modifier = Modifier.align(Alignment.BottomCenter),
-            onClick = { store.addItem(TodoItemData(title = "测试测试测试")) }
-        ) {
-            Text("添加 ")
-        }
+        NewItemInput(modifier = Modifier.align(Alignment.BottomCenter), handleAdd = { data -> store.addItem(data) })
     }
 }
 
@@ -85,10 +103,80 @@ fun TodoItem(data: TodoItemData, handleCheck: (id: UUID) -> Unit, handleRemove: 
             .border(1.dp, Color.LightGray)
             .fillMaxWidth()
     ) {
-        Text(data.title, modifier = Modifier.padding(start = 10.dp).weight(1f))
+        Text(data.title, modifier = Modifier
+            .padding(start = 10.dp)
+            .weight(1f))
         Checkbox(checked = data.status, onCheckedChange = { handleCheck(data.id) })
         TextButton(onClick = { handleRemove(data.id) }) {
-            Text(text = "删除")
+            Text(text = "❌")
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NewItemInput(modifier: Modifier = Modifier, handleAdd: (data: TodoItemData) -> Unit) {
+    val ctx = LocalContext.current
+    var text by remember {
+        mutableStateOf("")
+    }
+    var type by remember {
+        mutableStateOf(TodoType.Misc)
+    }
+
+    fun handleChange(str: String) {
+        text = str
+    }
+    @SuppressLint("ShowToast")
+    fun handleItemAdd() {
+        handleAdd(
+            TodoItemData(
+                status = false,
+                id = UUID.randomUUID(),
+                title = text,
+                content = "",
+                type = type
+            )
+        )
+        text = ""
+        Toast.makeText(ctx, "添加成功", Toast.LENGTH_SHORT).show()
+    }
+
+    Column(modifier = Modifier
+        .padding(8.dp)
+        .then(modifier)) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            TodoType.values().forEach { it ->
+                IconButton(onClick = { type = it }) {
+                    Icon(
+                        Icons.Filled.Home,
+                        contentDescription = "star",
+                        tint = if (type === it) Color.DarkGray else Color.LightGray
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier
+            .height(1.dp)
+            .background(Color.LightGray)
+            .fillMaxWidth())
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .then(modifier), verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = text,
+                placeholder = { Text("输入内容...") },
+                onValueChange = { handleChange(it) },
+                modifier = Modifier
+                    .padding(end = 10.dp),
+                colors = TextFieldDefaults.textFieldColors(containerColor = Color.Transparent),
+                maxLines = 1
+            )
+            Button(onClick = {handleItemAdd()}, enabled = text.isNotBlank()) {
+                Text(text = "添加")
+            }
+        }
+    }
+
 }
